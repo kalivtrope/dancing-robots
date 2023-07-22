1c8a390c71f246d0781810ba846c921d475c9cca
Kristýna Petrlíková <kalivtrope@gmail.com>
2023-07-23 01:20:27 +0200

    enums: move to separate file

---
+++ interpreter/enums.lua
@@ -0,0 +1,44 @@
+local ObjectChar
+
+local ObjectType = {
+  EMPTY = 1,
+  START = 2,
+  END = 3,
+  WALL = 4,
+  ITEM = 5,
+  from_char = function(c)
+    if not ObjectChar[c] then
+      error(string.format("unknown object type '%s'", c), 2)
+    end
+    return ObjectChar[c]
+  end
+}
+
+ObjectChar = {
+  ['.'] = ObjectType.EMPTY,
+  ['S'] = ObjectType.START,
+  ['E'] = ObjectType.END,
+  ['#'] = ObjectType.WALL,
+  ['I'] = ObjectType.ITEM,
+}
+
+return {
+  ObjectType = ObjectType,
+  ObjectChar = ObjectChar,
+  Direction = {
+    NORTH = 1,
+    EAST = 2,
+    SOUTH = 3,
+    WEST = 4,
+  },
+  GameType = {
+    MATRIX = 1,
+    MAZE = 2,
+    SORT = 3,
+  },
+  GameResult = {
+    NONE = 1,
+    WIN = 2,
+    LOSE = 3,
+  },
+}
