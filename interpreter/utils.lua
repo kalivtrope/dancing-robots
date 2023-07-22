93cc244eb033e882be7184fad568f8445bf9a945
Kristýna Petrlíková <kalivtrope@gmail.com>
2023-07-23 01:22:49 +0200

    utils: dump and basic assertions

---
+++ interpreter/utils.lua
@@ -0,0 +1,27 @@
+local function dump(o)
+  -- borrowed from: https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+return {
+  dump = dump,
+  assert_type = function (var, var_str, type_str)
+    assert(type(var) == type_str, string.format("%s must be a %s (got %s)", var_str, type_str, type(var)))
+  end,
+  assert_bounds = function (var, var_str, min, max)
+    if not max then
+      assert(min <= var, string.format("variable '%s' out of bounds [%d, inf]", var_str, min))
+    else
+      assert(min <= var and var <= max, string.format("variable '%s' out of bounds [%d, %d]", var_str, min, max))
+    end
+  end,
+}
