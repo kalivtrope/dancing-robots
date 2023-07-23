7f20b3b97816aa7558de2e3e0d639834a6308187
Kristýna Petrlíková <kalivtrope@gmail.com>
2023-07-24 00:24:17 +0200

    karel: file no longer used

--- interpreter/karel.lua
+++
@@ -1,8 +0,0 @@
-local Game = require("game")
-
-local filename = ... or arg[1]
-local file = filename and assert(io.open(filename, "r")) or io.stdin
-local game_type_str = file:read("*line")
-local game_height, game_width = file:read("*n", "*n")
-local game_maze_str = file:read("*all")
-curr_game = Game:new({height = game_height, width = game_width, type_str = game_type_str, maze_str = game_maze_str})
