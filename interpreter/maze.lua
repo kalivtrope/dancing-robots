8a6b7bea051aa2845dba1f2929204c8dc3ad9f21
Kristýna Petrlíková <kalivtrope@gmail.com>
2023-07-23 16:20:09 +0200

    maze: command handling

--- interpreter/maze.lua
+++ interpreter/maze.lua
@@ -171,6 +171,135 @@ local function maze_from_str(maze_str, height, width)
   return maze
 end
 
+local function move(x, y, dir)
+  local d_x, d_y = Direction.dir_delta(dir)
+  return x + d_x, y + d_y
+end
+
+function Maze:collect(x, y)
+  local was_last_item = self[x][y]:count_items() == 1
+  if was_last_item then
+    self.item_rows[y]:remove_item(x)
+    self.item_cols[x]:remove_item(y)
+  end
+  return self[x][y]:remove_item()
+end
+
+function Maze:drop(x, y)
+  local was_first_item = self[x][y]:count_items() == 0
+  if was_first_item then
+    self.item_rows[y]:add_item(x)
+    self.item_cols[x]:add_item(y)
+  end
+  return self[x][y]:add_object(ObjectType.Item)
+end
+
+function Maze:move_to_item(x, y, dir)
+  local wall_x, wall_y = self[x][y].neighbour_walls[dir].x, self[x][y].neighbour_walls[dir].y
+  if dir == Direction.NORTH then
+    if wall_y == y-1 then
+      return nil,nil,false,true
+    end
+    local item_y = self.item_cols[x]:index_of_lower_bound(y-1)
+    if wall_y > item_y then
+      return x,wall_y+1,false,false
+    end
+    if item_y < 1 or item_y > self.height then
+      error("maze is missing borders", 2)
+    end
+    return x,item_y,true,false
+  elseif dir == Direction.EAST then
+    if wall_x == x+1 then
+      return nil,nil,false,true
+    end
+    local lb_x = self.item_rows[y]:index_of_lower_bound(x)
+    local item_x = self.item_rows[y]:right(lb_x)
+    if wall_x < item_x then
+      return wall_x-1,y,false,false
+    end
+    if item_x < 1 or item_x > self.width then
+      error("maze is missing borders", 2)
+    end
+    return item_x,y,true,false
+  elseif dir == Direction.SOUTH then
+    if wall_y == y+1 then
+      return nil,nil,false,true
+    end
+    local lb_y = self.item_cols[x]:index_of_lower_bound(y)
+    local item_y = self.item_cols[x]:right(lb_y)
+    if wall_y < item_y then
+      return x,wall_y-1,false,false
+    end
+    if item_y < 1 or item_y > self.height then
+      error("maze is missing borders", 2)
+    end
+    return x,item_y,true,false
+  elseif dir == Direction.WEST then
+    if wall_x == x-1 then
+      return nil,nil,false,true
+    end
+    local item_x = self.item_rows[y]:index_of_lower_bound(x-1)
+    if wall_x > item_x then
+      return wall_x+1,y,false,false
+    end
+    if item_x < 1 or item_x > self.width then
+      error("maze is missing borders", 2)
+    end
+    return item_x,y,true,false
+  else
+    error("unknown direction " .. dir, 2)
+  end
+end
+
+local function one_block_before(x, y, dir)
+  return move(x, y, Direction.opposite_direction(dir))
+end
+
+function Maze:move_to_wall(x, y, dir)
+  local wall = self[x][y].neighbour_walls[dir]
+  if not wall then
+    return nil,nil,nil
+  end
+  local ret_x, ret_y = one_block_before(x, y, wall)
+  return ret_x,ret_y,true
+end
+
+local function normalize_delta(d_x, d_y)
+  return d_x ~= 0 and d_x // math.abs(d_x) or 0,
+         d_y ~= 0 and d_y // math.abs(d_y) or 0
+end
+
+local function reachable_from_pos(src_x, src_y, dst_x, dst_y, dir)
+  local diff_x, diff_y = normalize_delta(dst_x - src_x, dst_y - src_y)
+  local dir_x, dir_y = Direction.dir_delta(dir)
+  return dir_x == diff_x and dir_y == diff_y
+end
+
+local function is_closer(src_x, src_y, a_x, a_y, b_x, b_y, dir)
+  assert(reachable_from_pos(src_x, src_y, a_x, a_y, dir))
+  assert(reachable_from_pos(src_x, src_y, b_x, b_y, dir))
+  local d_ax, d_ay = normalize_delta(a_x-src_x,a_y-src_y)
+  local d_bx, d_by = normalize_delta(b_x-src_x,b_y-src_y)
+  return math.abs(d_ax) <= math.abs(d_bx) and math.abs(d_ay) <= math.abs(d_by)
+end
+
+function Maze:move_to_pos(src_x, src_y, dst_x, dst_y, dir)
+  local wall_x, wall_y, success = self:move_to_wall(src_x, src_y, dir)
+  if reachable_from_pos(src_x, src_y, dst_x, dst_y, dir)
+    and (not success or is_closer(src_x, src_y, dst_x, dst_y, wall_x, wall_y)) then
+    return dst_x, dst_y, true
+  end
+  return wall_x, wall_y, false
+end
+
+function Maze:move_to_start(x, y, dir)
+  return self:move_to_pos(x, y, self.start_cell.x, self.start_cell.y, dir)
+end
+
+function Maze:move_to_end(x, y, dir)
+  return self:move_to_pos(x, y, self.end_cell.x, self.end_cell.y, dir)
+end
+
 function Maze:new(params)
   -- params: table with entries:
     -- `maze_str` describes the textual representation of the maze
