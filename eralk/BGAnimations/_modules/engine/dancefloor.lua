762cf394b47333d5b79c8c63c506cad17796dce8
Kristýna Petrlíková <kalivtrope@gmail.com>
2023-08-08 00:10:42 +0200

    dancefloor: adapt to drawing smaller grids
    It was possible to draw smaller grids than of size
    max_cells_per_row x max_cells_per_col even before this commit,
    however now a smaller grid gets appropriately centered to the screen
    instead of sticking to the top and left borders of the maximum allowed
    grid.
    Oh and I also documented the core function draw_sprite_at_pos a little
    to explain how the game coordinate system and my "magical" positioning
    formula work while in the process of adapting it.

--- eralk/BGAnimations/_modules/engine/dancefloor.lua
+++ eralk/BGAnimations/_modules/engine/dancefloor.lua
@@ -18,10 +18,29 @@ local function CreateSprite(name)
   }
 end
 
-local function draw_sprite_at_pos(sprite, x, y)
+local function draw_sprite_at_pos(sprite, x, y, cells_per_row, cells_per_column)
   sprite:visible(true)
-  sprite:xy(real_size/2 + (x-1)*real_size - max_cells_per_row*real_size/2 + scx,
-            real_size/2 + (y-1)*real_size - max_cells_per_column*real_size/2 + scy)
+  -- Here the method sprite:xy() sets the position of a sprite's CENTER relative to the screen coordinates.
+  -- Screen coordinates go from (0,0) at the top-left corner to
+  --   (SCREEN_WIDTH,SCREEN_HEIGHT) at the bottom-right corner of the screen.
+  -- You may choose to resize the game window, however in that case the coordinate system stays the same
+  --   and the engine instead takes full care of resizing and scaling content accordingly.
+
+  -- First part (real_size/2): the sprite's actual calculated onscreen size is stored in real_size
+  --   (we assume for all sprites to be squares of dimensions real_size x real_size).
+  --   Since we're setting the position for the sprite's center, in order for the topleft corner sprite
+  --   to be at (0,0) we must offset the coordinates by real_size/2 in both x and y directions.
+  -- Second part ((x-1)*real_size): this marks the offset from the left/top edge of the leftmost/topmost placed sprite.
+  -- Third part (-cells_per_row*real_size/2 + scx): first off, the origin (at the top-left corner sprite) gets moved
+  --   to the screen center by adding scx.
+  --   Then, by assuming we're drawing cells_per_row number of sprites,
+  --   we shift the origin back by cells_per_row/2 and cells_per_column/2 sprites
+  --   in order for the sprites in middle column/row to be placed exactly at the screen center x/y
+  --   Subnote: it should always hold that 0 < cells_per_row <= max_cells_per_row
+  sprite:xy(real_size/2 + (x-1)*real_size - cells_per_row*real_size/2 + scx,
+            real_size/2 + (y-1)*real_size - cells_per_column*real_size/2 + scy)
+  -- In the end, we need to scale the sprite's texture to have real_size on screen, which is done by the value
+  --   of basezoom=real_size/texture_size (that is determined in the runtime below)
   sprite:basezoomx(basezoom)
   sprite:basezoomy(basezoom)
   sprite:Draw()
@@ -33,10 +52,16 @@ local cell_data = nil
 local function draw_function(self)
   if not cell_data then return end
   local curr_data = cell_data
+  local cells_per_column = #cell_data
+  local cells_per_row = #cell_data[1]
+  assert(cells_per_column > 0 and cells_per_column <= max_cells_per_column,
+    string.format("invalid number of cells per column (got '%d')", cells_per_column))
+  assert(cells_per_row > 0 and cells_per_row <= max_cells_per_row,
+    string.format("invalid number of cells per column (got '%d')", cells_per_row))
   for y,row in ipairs(curr_data) do
     for x,cell_type_arr in ipairs(row) do
       for _,cell_type in ipairs(cell_type_arr) do
-        draw_sprite_at_pos(self:GetChild(cell_type), x, y)
+        draw_sprite_at_pos(self:GetChild(cell_type), x, y, cells_per_row, cells_per_column)
       end
     end
   end
