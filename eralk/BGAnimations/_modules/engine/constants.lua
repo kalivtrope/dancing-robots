72e2b13ff991e9ae6543e044f16a025deed295f8
Kristýna Petrlíková <kalivtrope@gmail.com>
2023-08-10 16:57:01 +0200

    make dancefloor 2 cells wider to cover whole area

--- eralk/BGAnimations/_modules/engine/constants.lua
+++ eralk/BGAnimations/_modules/engine/constants.lua
@@ -1,7 +1,6 @@
-local max_maze_width = 1  -- a float in range (0,1] denoting the max percentage of width taken up by the maze
 local max_cells_per_column = 10
 local real_size = SCREEN_HEIGHT / max_cells_per_column
-local max_cells_per_row = SCREEN_WIDTH * max_maze_width // real_size
+local max_cells_per_row = SCREEN_WIDTH // real_size + 2
 local max_items_per_cell = #(require("engine.enums").ItemCountToDrawable)
 
 return {
