9a88fa4745d1d1ec8ccce3bd4becd957c088c73e
Kristýna Petrlíková <kalivtrope@gmail.com>
2023-08-28 19:02:25 +0200

    sortp generator: coordinate swaps

--- generators/sortp/main.lua
+++ generators/sortp/main.lua
@@ -26,14 +26,14 @@ end
 
 local function fill_column(grid, column_no, num_objs, column_height)
   for i=1,num_objs do
-    grid[2*column_no][column_height - 2*i + 1]:add_item()
+    grid[column_height - 2*i + 1][2*column_no]:add_item()
   end
 end
 
 
 local function fill_diagonal_from_pos(grid, start_x, start_y, lim_x, lim_y)
   while start_x <= lim_x and start_y <= lim_y do
-    grid[start_x][start_y]:add_wall()
+    grid[start_y][start_x]:add_wall()
     start_x = start_x + 1
     start_y = start_y + 1
   end
@@ -60,14 +60,14 @@ function SortpGenerator.generate(params)
   for i=1,n do
     fill_column(gen.grid, i, perm[i], total_n)
     if i < n then
-      gen.grid[2*i][total_n - 2*i - 2]:add_wall()
+      gen.grid[total_n - 2*i - 2][2*i]:add_wall()
     end
     fill_diagonal_from_pos(gen.grid, 2*i+1, total_n-2*i+1, total_n-1, total_n-1)
   end
   ---[[
-  local start_x_end_y, start_y_end_x = 2, total_n-1
-  gen.grid[start_x_end_y][start_y_end_x]:add_start()
-  gen.grid[start_y_end_x][start_x_end_y]:add_end()
+  local start_col_end_row, start_row_end_col = 2, total_n-1
+  gen.grid[start_row_end_col][start_col_end_row]:add_start()
+  gen.grid[start_col_end_row][start_row_end_col]:add_end()
   --]]
   return tostring(gen)
 end
